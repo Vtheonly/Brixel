183de03d2d6272eda4a452b25be19ae53fac62c1
mersel fares <merselfaresw@gmail.com>
2025-09-18 22:23:56 +0100

    gtg

---
+++ src/app/(main)/case-studies/[slug]/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import React from 'react';
+
+const CaseStudySlugPage = () => {
+  return (
+    <div>
+      <h1>Case Study Post</h1>
+      <p>This is a placeholder for a case study post.</p>
+    </div>
+  );
+};
+
+export default CaseStudySlugPage;
